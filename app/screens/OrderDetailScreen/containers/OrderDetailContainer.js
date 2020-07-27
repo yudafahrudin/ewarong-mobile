ab00c5294d9a392463b907e500fccf7ab092200b
yudafahrudin <ahmadyudafahrudin@gmail.com>
2020-07-27 10:36:45 +0700

    Remove api key

--- app/screens/OrderDetailScreen/containers/OrderDetailContainer.js
+++ app/screens/OrderDetailScreen/containers/OrderDetailContainer.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/named */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   View,
   FlatList,
@@ -7,9 +7,9 @@ import {
   ActivityIndicator,
   ScrollView,
 } from 'react-native';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {ListItem, Text, Input, Button} from 'react-native-elements';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ListItem, Text, Input, Button } from 'react-native-elements';
 import _ from 'lodash';
 import moment from 'moment';
 import {
@@ -28,10 +28,11 @@ class OrderDetailContainer extends Component {
     modalVisible: false,
     ewarong: null,
     disabled: false,
+    notes: '',
   };
 
   async componentDidMount() {
-    const {ewarong} = this.props;
+    const { ewarong } = this.props;
     this.setState({
       ewarong,
     });
@@ -40,7 +41,7 @@ class OrderDetailContainer extends Component {
   keyExtractor = (item, index) => index.toString();
 
   onChangeQTY = (qty, item) => {
-    const {orders} = this.state;
+    const { orders } = this.state;
     let dataOrder = [];
     if (qty <= item.qty) {
       dataOrder[item.id] = {
@@ -71,8 +72,8 @@ class OrderDetailContainer extends Component {
   };
 
   async orders() {
-    const {ewarong, orders} = this.state;
-    const {actions, navigate} = this.props;
+    const { ewarong, orders } = this.state;
+    const { actions, navigate } = this.props;
     console.log(Object.values(orders));
     this.setState({
       disabled: true,
@@ -100,15 +101,15 @@ class OrderDetailContainer extends Component {
   }
 
   renderItem = (props) => {
-    const {item} = props;
+    const { item } = props;
     console.log(item);
     return (
       <ListItem
         title={item.item.nama}
-        titleStyle={{fontSize: 18, fontWeight: 'bold'}}
+        titleStyle={{ fontSize: 18, fontWeight: 'bold' }}
         subtitle={
-          <View style={{flexDirection: 'row'}}>
-            <View style={{flex: 2}}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 2 }}>
               <Text>Total : {item.qty}</Text>
               <Text>
                 Satuan : {item.satuan_number} {item.satuan.nama}{' '}
@@ -124,15 +125,21 @@ class OrderDetailContainer extends Component {
   };
 
   async confirmOrders(params) {
-    const {navigate, detailOrder, actions, user} = this.props;
+    const { notes } = this.state;
+    params.notes = notes;
+    const { navigate, detailOrder, actions, user } = this.props;
     await actions.confirmOrder(params);
     await actions.getMyOrders();
     navigate('OrderListScreen');
   }
 
+  async openModal() {
+    this.setState({ modalVisible: true })
+  }
+
   render() {
-    const {navigate, detailOrder, actions, user} = this.props;
-    const {ewarong, orders, disabled, modalVisible} = this.state;
+    const { navigate, detailOrder, actions, user } = this.props;
+    const { ewarong, orders, disabled, modalVisible } = this.state;
     console.log('detailOrder', detailOrder);
     return (
       <ScrollView
@@ -140,59 +147,113 @@ class OrderDetailContainer extends Component {
           flex: 1,
           width: Dimension.DEVICE_WIDTH,
         }}>
-        <View style={{flexDirection: 'row', margin: 20}}>
-          <Text style={{flex: 1}}>Nomor Pesanan </Text>
-          <Text style={{}}>{detailOrder.nomor_pemesanan} </Text>
+        <Modal isVisible={modalVisible} style={{ justifyContent: 'center' }}>
+          <View
+            style={{
+              height: 200,
+              width: Dimension.DEVICE_WIDTH / 1.1,
+              alignSelf: 'center',
+              padding: 10,
+              borderRadius: 10,
+              backgroundColor: '#FFFFFF',
+            }}>
+            <View style={{ flex: 1, marginBottom: 10 }}>
+              <Input
+                placeholder="Tulisa catatan"
+                autoCapitalize="none"
+                keyboardAppearance="light"
+                autoFocus={false}
+                autoCorrect={false}
+                returnKeyType="next"
+                onChangeText={(val) =>
+                  this.setState({
+                    notes: val
+                  })
+                }
+              />
+            </View>
+            <Button
+              title={'LANJUTKAN REJECT'}
+              onPress={() => this.confirmOrders({
+                pemesanan_id: detailOrder.id,
+                status: 'REJECTED',
+              })}
+              buttonStyle={{
+                margin: 10,
+              }}
+            />
+            <Button
+              title={'CANCEL'}
+              onPress={() =>
+                this.setState({ modalVisible: false, disabled: false })
+              }
+              buttonStyle={{
+                backgroundColor: Colors.RED,
+                margin: 10,
+                marginTop: 0,
+              }}
+            />
+          </View>
+        </Modal>
+        <View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Nomor Pesanan </Text>
+          <Text>{detailOrder.nomor_pemesanan} </Text>
         </View>
-        <View style={{flexDirection: 'row', margin: 20}}>
-          <Text style={{flex: 1}}>Tanggal </Text>
-          <Text style={{}}>
+        <View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Tanggal </Text>
+          <Text>
             {moment(detailOrder.created_at)
               .add(7, 'hours')
               .format('DD-MM-YYYY hh:mm')}
           </Text>
         </View>
-        <View style={{margin: 20}}>
+        <View style={{ margin: 20 }}>
           {user.access_type == 'umum' ? (
-            <View style={{flexDirection: 'row'}}>
-              <Text style={{flex: 1}}>Kios </Text>
-              <Text style={{}}>{detailOrder.ewarong.nama_kios} </Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ flex: 1 }}>Kios </Text>
+              <Text>{detailOrder.ewarong.nama_kios} </Text>
             </View>
           ) : (
-            <View style={{flexDirection: 'row'}}>
-              <Text style={{flex: 1}}>Pembeli </Text>
-              <Text style={{}}>{detailOrder.user.name} </Text>
-            </View>
-          )}
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ flex: 1 }}>Pembeli </Text>
+                <Text>{detailOrder.user.name} </Text>
+              </View>
+            )}
         </View>
-        <View style={{flexDirection: 'row', margin: 20}}>
-          <Text style={{flex: 1}}>Total Qty </Text>
-          <Text style={{}}>{detailOrder.qty_total} </Text>
+        <View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Total Qty </Text>
+          <Text>{detailOrder.qty_total} </Text>
         </View>
-        <View style={{flexDirection: 'row', margin: 20}}>
-          <Text style={{flex: 1}}>Harga Total</Text>
+        <View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Harga Total</Text>
           <Text style={{}}>
             RP. {(detailOrder.harga_total / 1000).toFixed(3)}{' '}
           </Text>
         </View>
-        <View style={{flexDirection: 'row', margin: 20}}>
-          <Text style={{flex: 1}}>Status</Text>
+        {detailOrder.status == 'REJECTED' ? (<View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Catatan Penjual</Text>
+          <Text>
+            {detailOrder.notes ? detailOrder.notes : '( tidak ada catatan )'}
+          </Text>
+        </View>) : null}
+        <View style={{ flexDirection: 'row', margin: 20 }}>
+          <Text style={{ flex: 1 }}>Status</Text>
           {detailOrder.status != 'FINISH' &&
-          detailOrder.status != 'REJECTED' ? (
-            moment().diff(detailOrder.created_at, 'hours') - 7 > 4 ? (
-              <View>
-                <Text>EXPIRED</Text>
-              </View>
+            detailOrder.status != 'REJECTED' ? (
+              moment().diff(detailOrder.created_at, 'hours') - 7 > 4 ? (
+                <View>
+                  <Text>EXPIRED</Text>
+                </View>
+              ) : (
+                  <View>
+                    <Text>{detailOrder.status} </Text>
+                  </View>
+                )
             ) : (
               <View>
                 <Text>{detailOrder.status} </Text>
               </View>
-            )
-          ) : (
-            <View>
-              <Text>{detailOrder.status} </Text>
-            </View>
-          )}
+            )}
         </View>
         <FlatList
           keyExtractor={this.keyExtractor}
@@ -201,105 +262,88 @@ class OrderDetailContainer extends Component {
           renderItem={this.renderItem}
         />
         {detailOrder.status != 'REJECTED' &&
-        detailOrder.status != 'FINISH' &&
-        user.access_type == 'umum' ? (
-          <View style={{padding: 20}}>
-            {moment().diff(detailOrder.created_at, 'hours') - 7 > 4 ? (
-              <Text style={{fontSize: 20, textAlign: 'center'}}>
-                Pesanan Kadaluarsa
-              </Text>
-            ) : (
-              <View>
-                <Text style={{fontSize: 18, textAlign: 'center'}}>
-                  Harap mengambil pesanan sebelum jam
+          detailOrder.status != 'FINISH' &&
+          user.access_type == 'umum' ? (
+            <View style={{ padding: 20 }}>
+              {moment().diff(detailOrder.created_at, 'hours') - 7 > 4 ? (
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>
+                  Pesanan Kadaluarsa
                 </Text>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                    color: Colors.RED,
-                  }}>
-                  {moment(detailOrder.created_at)
-                    .add(11, 'hours')
-                    .format('DD-MM-YYYY hh:mm:ss')}
+              ) : (
+                  <View>
+                    <Text style={{ fontSize: 18, textAlign: 'center' }}>
+                      Harap mengambil pesanan sebelum jam
                 </Text>
-              </View>
-            )}
-          </View>
-        ) : null}
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        color: Colors.RED,
+                      }}>
+                      {moment(detailOrder.created_at)
+                        .add(11, 'hours')
+                        .format('DD-MM-YYYY hh:mm:ss')}
+                    </Text>
+                  </View>
+                )}
+            </View>
+          ) : null}
 
-        {user.access_type == 'rpk' && detailOrder.status == 'CONFIRM' ? (
-          <View>
-            <Button
-              title={'FINISH'}
-              onPress={() =>
-                this.confirmOrders({
-                  pemesanan_id: detailOrder.id,
-                  status: 'FINISH',
-                })
-              }
-              disabled={disabled}
-              buttonStyle={{
-                width: Dimension.DEVICE_WIDTH - 20,
-                margin: 10,
-                marginTop: 0,
-              }}
-            />
-          </View>
-        ) : null}
+        {user.access_type == 'rpk' && detailOrder.status == 'CONFIRM' &&
+          moment().diff(detailOrder.created_at, 'hours') - 7 <= 4 ? (
+            <View>
+              <Button
+                title={'FINISH'}
+                onPress={() =>
+                  this.confirmOrders({
+                    pemesanan_id: detailOrder.id,
+                    status: 'FINISH',
+                  })
+                }
+                disabled={disabled}
+                buttonStyle={{
+                  width: Dimension.DEVICE_WIDTH - 20,
+                  margin: 10,
+                  marginTop: 0,
+                }}
+              />
+            </View>
+          ) : null}
         {user.access_type == 'rpk' &&
-        detailOrder.status == 'OPEN' &&
-        moment().diff(detailOrder.created_at, 'hours') - 7 <= 4 ? (
-          <View>
-            <Button
-              title={'FINISH'}
-              onPress={() =>
-                this.confirmOrders({
-                  pemesanan_id: detailOrder.id,
-                  status: 'FINISH',
-                })
-              }
-              disabled={disabled}
-              buttonStyle={{
-                width: Dimension.DEVICE_WIDTH - 20,
-                margin: 10,
-                marginTop: 0,
-              }}
-            />
-            <Button
-              title={'KONFIRMASI'}
-              onPress={() =>
-                this.confirmOrders({
-                  pemesanan_id: detailOrder.id,
-                  status: 'CONFIRM',
-                })
-              }
-              disabled={disabled}
-              buttonStyle={{
-                width: Dimension.DEVICE_WIDTH - 20,
-                margin: 10,
-                marginTop: 0,
-              }}
-            />
-            <Button
-              title={'REJECT'}
-              onPress={() =>
-                this.confirmOrders({
-                  pemesanan_id: detailOrder.id,
-                  status: 'REJECTED',
-                })
-              }
-              disabled={disabled}
-              buttonStyle={{
-                backgroundColor: Colors.RED,
-                width: Dimension.DEVICE_WIDTH - 20,
-                margin: 10,
-                marginTop: 0,
-              }}
-            />
-          </View>
-        ) : null}
+          detailOrder.status == 'OPEN' &&
+          moment().diff(detailOrder.created_at, 'hours') - 7 <= 4 ? (
+            <View>
+              <Button
+                title={'KONFIRMASI'}
+                onPress={() =>
+                  this.confirmOrders({
+                    pemesanan_id: detailOrder.id,
+                    status: 'CONFIRM',
+                  })
+                }
+                disabled={disabled}
+                buttonStyle={{
+                  width: Dimension.DEVICE_WIDTH - 20,
+                  margin: 10,
+                  marginTop: 0,
+                }}
+              />
+              <Button
+                title={'REJECT'}
+                onPress={() =>
+                  this.openModal()
+                }
+                disabled={disabled}
+                buttonStyle={{
+                  backgroundColor: Colors.RED,
+                  width: Dimension.DEVICE_WIDTH - 20,
+                  margin: 10,
+                  marginTop: 0,
+                }}
+              />
+            </View>
+          ) : null}
         <Button
           title={'KEMBALI KE HOME'}
           onPress={() => navigate('HomeScreen')}
